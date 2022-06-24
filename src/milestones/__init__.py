54629766945d1879af6359274bb3abd3b9b75191
Peter Würth <wuerth.peter@freenet.de>
2022-06-24 15:18:16 +0200

    add milestone 4

--- src/milestones/__init__.py
+++ src/milestones/__init__.py
@@ -4,6 +4,8 @@ def add_subcommands(subparsers: argparse._SubParsersAction):
     from .m1 import args as m1_args
     from .m2 import args as m2_args
     from .m3 import args as m3_args
+    from .m4 import args as m4_args
     m1_args(subparsers)
     m2_args(subparsers)
     m3_args(subparsers)
+    m4_args(subparsers)
