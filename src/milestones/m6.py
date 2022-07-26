correct calculation of characteristic length for Reynolds number

src/milestones/m6.py
@@ -64,8 +64,7 @@ def main(args: argparse.Namespace):
         args.viscosity = omega_to_viscosity(np.array(args.omega))
         args.oemga = None
     # check reynolds_nr, viscosity, wall_velocity
-    # TODO: what is L in case size_y != size_x?
-    L = args.size_y
+    L = max(args.size_x, args.size_y)
     if args.reynolds_nr and args.viscosity and args.wall_velocity:
         raise ValueError("--reynolds_nr, --viscosity|--omega and --wall_velocity can't be present at the same time")
     elif args.reynolds_nr and args.viscosity:

src/milestones/m7.py
@@ -93,8 +93,7 @@ def main(args: argparse.Namespace):
 
 
 def plot_velocity_field(filename: str, args: argparse.Namespace, lattice: sim.LatticeBoltzmann, velocity: np.ndarray):
-    # TODO: what is L in case size_y != size_x?
-    L = args.size_y
+    L = max(args.size_x, args.size_y)
     Re = L * args.wall_velocity / lattice.viscosity
 
     fig, ax = plt.subplots(figsize=(6.8, 6.0), dpi=100)